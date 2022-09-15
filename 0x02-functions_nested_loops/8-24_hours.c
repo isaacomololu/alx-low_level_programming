1a99c40c016065c2552f9886bba5efb9c4491e3d
Isaac Omololu <isaaccassij.omo@gmail.com>
2022-09-14 18:26:42 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,35 @@
 
 voiid jack_bauer(void)
 {
-	int h,m;
-	for (h =0;h<24;h++)
+	int i, j;
+
+	for (i = 0; i < 24; i++)
 	{
-		for (m=0;m<60;m++)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar((h/10)='0');
-			_putchar((h%10)+'0');
-			_putchar(':');
-			_putchar((m/10)+'0');
-			_putchar((m%10)+'0');
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j >= 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
 			_putchar('\n');
 		}
 	}
-}
+}	
