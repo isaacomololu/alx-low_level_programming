422a5c00a3dfd70a3f9739cefd206782319ecd24
Isaac Omololu <isaaccassij.omo@gmail.com>
2022-09-21 15:11:59 -0700

    try

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,9 @@
  * @s1: param1
  * @s2: param2
  * Return:
- * 	returns zero if s1 == s2
- * 	returns negative number if s1 < s2
- * 	returns positive number if s1 > s2
+ *	returns zero if s1 == s2
+ *	returns negative number if s1 < s2
+ *	returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
