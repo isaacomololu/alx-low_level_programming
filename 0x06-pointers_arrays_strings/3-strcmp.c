c38c7ad83fd0c01909f9c5f91df0b2c5019b8e57
Isaac Omololu <isaaccassij.omo@gmail.com>
2022-09-21 15:05:26 -0700

    try again

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,37 @@
  * _strcmp -> for comparing purpose
  * @s1: param1
  * @s2: param2
- * Return: integer
+ * Return:
+ * 	returns zero if s1 == s2
+ * 	returns negative number if s1 < s2
+ * 	returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-	
+	int i = 0, diff = 0;
 
-	result = strcmp(s1, s2);
-	return (result);
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (diff);
 }
